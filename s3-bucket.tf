9605abe21348ddbb3f35ce978884fff374dfe8b0
cand0 <rudtjr7655@gmail.com>
2023-12-02 02:42:17 +0000

    asdf

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -2,9 +2,8 @@ module "s3_bucket" {
   source = "terraform-aws-modules/s3-bucket/aws"
   version = "2.8.0"
 
-  bucket = "my-s3-bucketi-cand123"
   acl    = "private"
-  bucket_prefix = "cand1"
+  bucket_prefix = "cand1-asdfzxcv1"
   versioning = {
     enabled = true
   }
