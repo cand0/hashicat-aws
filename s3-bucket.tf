b29b0915d9321479e58d1fc4bcf079ed56974ce2
cand0 <rudtjr7655@gmail.com>
2023-12-02 02:37:38 +0000

    test

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,6 +1,5 @@
 module "s3-bucket" {
   source  = "app.terraform.io/hashicat-aws-cand0/s3-bucket/aws"
   version = "2.8.0"
-  bucket_prefix = "test"
-  prefix = "test2"
+  bucket_prefix = "cand123-test"
 }
